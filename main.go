ea7c4b414a60d65a39b84d0afd8edeb4eb2b8e12
Pablo <pablo1@mailbox.org>
2018-10-12 14:42:28 +0200

    added tests

--- main.go
+++ main.go
@@ -26,6 +26,9 @@ func main() {
 	g.Set(10, 11, true)
 
 	for i := 0; i < 10; i++ {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
 		g.Print()
 		fmt.Print("Generation: ")
 		fmt.Println(i)
@@ -58,33 +61,61 @@ func (gb *GameBoard) RandInit(numAlive int) {
 
 func (gb *GameBoard) Iterate() {
 
-	gbOld := gb
+	gbOld := NewGameBoard(gb.xSize, gb.ySize)
+	gbOld.cells = gb.cells
 
 	for y := 0; y < gb.ySize; y++ { // Rows
 		for x := 0; x < gb.xSize; x++ { // Collumns
 
-			// Eine tote Zelle mit genau drei lebenden Nachbarn wird in der Folgegeneration neu geboren.
+			// Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
 			if !gbOld.Get(x, y) && gbOld.Neighbours(x, y) == 3 {
 				gb.Set(x, y, true)
+				continue
 			}
 
-			// Lebende Zellen mit weniger als zwei lebenden Nachbarn sterben in der Folgegeneration an Einsamkeit.
+			// Any live cell with fewer than two live neighbors dies, as if by underpopulation.
 			if gbOld.Get(x, y) && gbOld.Neighbours(x, y) < 2 {
 				gb.Set(x, y, false)
+				continue
 			}
 
-			// Eine lebende Zelle mit zwei oder drei lebenden Nachbarn bleibt in der Folgegeneration am Leben.
+			// Any live cell with two or three live neighbors lives on to the next generation.
+			if gbOld.Get(x, y) && ((gbOld.Neighbours(x, y) == 2) || (gbOld.Neighbours(x, y) == 3)) {
+				//No need to set, already alive
+				continue
+			}
 
-			// Lebende Zellen mit mehr als drei lebenden Nachbarn sterben in der Folgegeneration an Überbevölkerung.
-			if gbOld.Neighbours(x, y) > 3 {
+			// Any live cell with more than three live neighbors dies, as if by overpopulation.
+			if gbOld.Get(x, y) && (gbOld.Neighbours(x, y) > 3) {
 				gb.Set(x, y, false)
+				continue
 			}
 		}
 	}
+}
 
+func (gb *GameBoard) Equal(gb2 *GameBoard) bool {
+	if gb.xSize != gb2.xSize || gb.ySize != gb2.ySize {
+		return false
+	}
+
+	if len(gb.cells) != len(gb2.cells) {
+		return false
+	}
+
+	for k := range gb.cells {
+		if gb.cells[k] != gb2.cells[k] {
+			return false
+		}
+	}
+
+	return true
 }
 
 func (gb *GameBoard) Set(x, y int, val bool) {
+	if !gb.InBounds(x, y) {
+
+	}
 	if x > gb.xSize-1 || x < 0 || y > gb.ySize-1 || y < 0 {
 		log.Fatal("Invalid Coordinate")
 	}
@@ -118,22 +149,38 @@ func (gb *GameBoard) Neighbours(x, y int) int {
 }
 
 func (gb *GameBoard) InBounds(x int, y int) bool {
-	return (x >= 0 && x < gb.xSize && y > 0 && y < gb.ySize)
+	return (x >= 0 &&
+		x < gb.xSize &&
+		y >= 0 &&
+		y < gb.ySize)
 }
 
 func (gb *GameBoard) Print() {
+	//Top margin
+	fmt.Print("╔")
+	for x := 1; x <= gb.xSize; x++ {
+		fmt.Print("═")
+	}
+	fmt.Println("╗")
 
-	cmd := exec.Command("clear")
-	cmd.Stdout = os.Stdout
-	cmd.Run()
-	for y := 0; y < gb.ySize; y++ { // Rows
-		for x := 0; x < gb.xSize; x++ { // Collumns
+	// Rows
+	for y := 0; y < gb.ySize; y++ {
+		fmt.Print("║")
+		// Collumns
+		for x := 0; x < gb.xSize; x++ {
 			if gb.Get(x, y) {
 				fmt.Print("█")
 			} else {
 				fmt.Print(" ")
 			}
 		}
-		fmt.Println("")
+		fmt.Println("║")
 	}
+
+	//Bottom margin
+	fmt.Print("╚")
+	for x := 1; x <= gb.xSize; x++ {
+		fmt.Print("═")
+	}
+	fmt.Println("╝")
 }
--- main_test.go
+++ main_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestGameBoard_Neighbours(t *testing.T) {
 
@@ -8,7 +11,6 @@ func TestGameBoard_Neighbours(t *testing.T) {
 		false, false, false, false, false,
 		false, false, false, false, false,
 		false, false, true, false, false,
-		false, false, false, false, false,
 		false, false, false, false, false,
 		false, false, false, false, false,
 	}}
@@ -19,14 +21,12 @@ func TestGameBoard_Neighbours(t *testing.T) {
 		false, false, true, false, false,
 		false, false, false, false, false,
 		false, false, false, false, false,
-		false, false, false, false, false,
 	}}
 
 	gb2 := &GameBoard{generation: 0, xSize: 5, ySize: 5, cells: []bool{
 		false, false, false, false, false,
 		false, true, true, false, false,
 		false, false, true, false, false,
-		false, false, false, false, false,
 		false, false, false, false, false,
 		false, false, false, false, false,
 	}}
@@ -37,14 +37,12 @@ func TestGameBoard_Neighbours(t *testing.T) {
 		false, false, true, false, false,
 		false, false, false, false, false,
 		false, false, false, false, false,
-		false, false, false, false, false,
 	}}
 
 	gb4 := &GameBoard{generation: 0, xSize: 5, ySize: 5, cells: []bool{
 		false, false, false, false, false,
 		false, true, true, true, false,
 		false, true, true, false, false,
-		false, false, false, false, false,
 		false, false, false, false, false,
 		false, false, false, false, false,
 	}}
@@ -55,7 +53,6 @@ func TestGameBoard_Neighbours(t *testing.T) {
 		false, true, true, true, false,
 		false, false, false, false, false,
 		false, false, false, false, false,
-		false, false, false, false, false,
 	}}
 
 	gb6 := &GameBoard{generation: 0, xSize: 5, ySize: 5, cells: []bool{
@@ -63,7 +60,6 @@ func TestGameBoard_Neighbours(t *testing.T) {
 		false, true, true, true, false,
 		false, true, true, true, false,
 		false, true, false, false, false,
-		false, false, false, false, false,
 		false, false, false, false, false,
 	}}
 
@@ -73,7 +69,6 @@ func TestGameBoard_Neighbours(t *testing.T) {
 		false, true, true, true, false,
 		false, true, true, false, false,
 		false, false, false, false, false,
-		false, false, false, false, false,
 	}}
 
 	gb8 := &GameBoard{generation: 0, xSize: 5, ySize: 5, cells: []bool{
@@ -82,14 +77,12 @@ func TestGameBoard_Neighbours(t *testing.T) {
 		false, true, true, true, false,
 		false, true, true, true, false,
 		false, false, false, false, false,
-		false, false, false, false, false,
 	}}
 
 	//corner top left
 
 	gb9 := &GameBoard{generation: 0, xSize: 5, ySize: 5, cells: []bool{
 		true, false, false, false, false,
-		false, false, false, false, false,
 		false, false, false, false, false,
 		false, false, false, false, false,
 		false, false, false, false, false,
@@ -102,13 +95,11 @@ func TestGameBoard_Neighbours(t *testing.T) {
 		false, false, false, false, false,
 		false, false, false, false, false,
 		false, false, false, false, false,
-		false, false, false, false, false,
 	}}
 
 	gb11 := &GameBoard{generation: 0, xSize: 5, ySize: 5, cells: []bool{
 		true, true, false, false, false,
 		true, false, false, false, false,
-		false, false, false, false, false,
 		false, false, false, false, false,
 		false, false, false, false, false,
 		false, false, false, false, false,
@@ -120,12 +111,10 @@ func TestGameBoard_Neighbours(t *testing.T) {
 		false, false, false, false, false,
 		false, false, false, false, false,
 		false, false, false, false, false,
-		false, false, false, false, false,
 	}}
 
 	//corner bottom right
 	gb13 := &GameBoard{generation: 0, xSize: 5, ySize: 5, cells: []bool{
-		false, false, false, false, false,
 		false, false, false, false, false,
 		false, false, false, false, false,
 		false, false, false, false, false,
@@ -137,7 +126,6 @@ func TestGameBoard_Neighbours(t *testing.T) {
 		false, false, false, false, false,
 		false, false, false, false, false,
 		false, false, false, false, false,
-		false, false, false, false, false,
 		false, false, false, true, false,
 		false, false, false, false, true,
 	}}
@@ -146,13 +134,11 @@ func TestGameBoard_Neighbours(t *testing.T) {
 		false, false, false, false, false,
 		false, false, false, false, false,
 		false, false, false, false, false,
-		false, false, false, false, false,
 		false, false, false, true, true,
 		false, false, false, false, true,
 	}}
 
 	gb16 := &GameBoard{generation: 0, xSize: 5, ySize: 5, cells: []bool{
-		false, false, false, false, false,
 		false, false, false, false, false,
 		false, false, false, false, false,
 		false, false, false, false, false,
@@ -193,6 +179,137 @@ func TestGameBoard_Neighbours(t *testing.T) {
 			if got := tt.gb.Neighbours(tt.x, tt.y); got != tt.want {
 				tt.gb.Print()
 				t.Errorf("GameBoard.Neighbours() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGameBoard_InBounds(t *testing.T) {
+
+	gb := NewGameBoard(5, 5)
+	tests := []struct {
+		name string
+		gb   *GameBoard
+		x    int
+		y    int
+		want bool
+	}{
+		{"Top Left", gb, 0, 0, true},
+		{"Top Right", gb, 4, 0, true},
+		{"Bottom Left", gb, 0, 4, true},
+		{"Bottom Right", gb, 4, 4, true},
+
+		{"Out Left", gb, -1, 0, false},
+		{"Out Right", gb, 5, 0, false},
+		{"Out Top", gb, 0, -1, false},
+		{"Out Bottom", gb, 5, 0, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gb := tt.gb
+			if got := gb.InBounds(tt.x, tt.y); got != tt.want {
+				t.Errorf("GameBoard.InBounds() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGameBoard_Iterate(t *testing.T) {
+
+	gb0 := NewGameBoard(5, 5)
+	gb1 := NewGameBoard(5, 5)
+	gb2 := NewGameBoard(5, 5)
+	gb3 := NewGameBoard(5, 5)
+
+	gb0want := NewGameBoard(5, 5)
+	gb1want := NewGameBoard(5, 5)
+	gb2want := NewGameBoard(5, 5)
+	gb3want := NewGameBoard(5, 5)
+
+	tests := []struct {
+		name string
+		gb   *GameBoard
+		want *GameBoard
+	}{
+		{"Any live cell with fewer than two live neighbors dies, as if by underpopulation.", gb0, gb0want},
+		{"Any live cell with two or three live neighbors lives on to the next generation.", gb1, gb1want},
+		{"Any live cell with more than three live neighbors dies, as if by overpopulation.", gb2, gb2want},
+		{"Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.", gb3, gb3want},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.gb.Iterate()
+
+			if !tt.gb.Equal(tt.want) {
+				t.Errorf("GameBoard.Iterate() = %v, want %v", tt.gb, tt.want)
+				fmt.Println("Got:")
+				tt.gb.Print()
+				fmt.Println("Want:")
+				tt.want.Print()
+			}
+		})
+	}
+}
+
+func TestGameBoard_Equal(t *testing.T) {
+
+	// Equal, all dead
+	gb0a := NewGameBoard(5, 5)
+	gb0b := NewGameBoard(5, 5)
+
+	// Different xSize
+	gb1a := NewGameBoard(4, 5)
+	gb1b := NewGameBoard(5, 5)
+
+	// Different ySize
+	gb2a := NewGameBoard(5, 5)
+	gb2b := NewGameBoard(5, 6)
+
+	// Different xSize and ySize
+	gb3a := NewGameBoard(5, 3)
+	gb3b := NewGameBoard(3, 5)
+
+	// Different cell length
+	gb4a := NewGameBoard(5, 5)
+	gb4b := NewGameBoard(5, 5)
+	gb4b.cells = []bool{false, false, false}
+
+	// Different cell(s)
+	gb5a := NewGameBoard(5, 5)
+	gb5b := NewGameBoard(5, 5)
+	gb5b.Set(0, 0, true)
+
+	// Equal, some alive
+	gb6a := NewGameBoard(5, 5)
+	gb6a.Set(0, 0, true)
+	gb6a.Set(1, 0, true)
+	gb6a.Set(2, 4, true)
+	gb6a.Set(3, 2, true)
+
+	gb6b := NewGameBoard(5, 5)
+	gb6b.Set(0, 0, true)
+	gb6b.Set(1, 0, true)
+	gb6b.Set(2, 4, true)
+	gb6b.Set(3, 2, true)
+
+	tests := []struct {
+		name string
+		gba  *GameBoard
+		gbb  *GameBoard
+		want bool
+	}{
+		{"Equal, all dead", gb0a, gb0b, true},
+		{"Different xSize", gb1a, gb1b, false},
+		{"Different ySize", gb2a, gb2b, false},
+		{"Different xSize and ySize", gb3a, gb3b, false},
+		{"Different cell length", gb4a, gb4b, false},
+		{"Different cells", gb5a, gb5b, false},
+		{"Equal, some alive ", gb6a, gb6b, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.gba.Equal(tt.gbb); got != tt.want {
+				t.Errorf("GameBoard.Equal() = %v, want %v", got, tt.want)
 			}
 		})
 	}
